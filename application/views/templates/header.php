e920ffabac3a64d8ee9a4292be37dd049439edd1
FlorianNelles <floriannelles@gmx.de>
2021-05-18 13:19:11 +0200

    Initial commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+
+	<title>Test_V3</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-sm navbar-light">
+	<a class="navbar-brand" >Test_v3</a>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="<?php echo base_url(); ?>">Home</a>
+			</li>
+
+			<li class="nav-item active">
+				<a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
+			</li>
+		</ul>
+
+		<ul class="nav navbar-nav navbar-right">
+		</ul>
+
+	</div>
+</nav>
+
+<div class="container">
